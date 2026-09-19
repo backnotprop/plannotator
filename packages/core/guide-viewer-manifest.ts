cee5b59149d41ff4affa5beb99bb1bb6133c4cec
katya4oyu <katya4oyu@gmail.com>
2026-09-19 11:18:34 +0900

    chore: sync guide viewer manifest after toolbar CSS change

--- packages/core/guide-viewer-manifest.ts
+++ packages/core/guide-viewer-manifest.ts
@@ -6,9 +6,9 @@ import type { GuideViewerAssets } from "./guide-format";
 
 export const GUIDE_VIEWER_MANIFEST: Omit<GuideViewerAssets, "baseUrl"> = {
   js: "viewer.48O72WuZ.js",
-  css: "viewer.DilvVn0i.css",
+  css: "viewer.D_1E9tIP.css",
   jsIntegrity: "sha384-XQrIafIUrC1JvOSLEFyhqNObv7YmydTDPwXCDJvSV7UcU2BMouDtQADclC1uvAGK",
-  cssIntegrity: "sha384-EvOFwI14FdyDrbnpahJjjIRkQXWl7ihnmSB4mDmFdIJCrdKTB3KfEAN+wfMNT9fL",
+  cssIntegrity: "sha384-BDcYChsOaSxe2NkqUvhsgzh/xJ850TPhJ0WiAHk6+L+ZOdsqDWx22bUZ99oBaZ5u",
   langs: {
     "astro": "chunks/astro.Ts5EKq2l.js",
     "c": "chunks/c.BIGW1oBm.js",
