04a63f3ceca13d9f328280381ad5d756f042163f
Michael Ramos <support@plannotator.ai>
2026-09-21 00:41:52 -0700

    chore: regenerate guide viewer manifest

--- packages/core/guide-viewer-manifest.ts
+++ packages/core/guide-viewer-manifest.ts
@@ -5,10 +5,10 @@
 import type { GuideViewerAssets } from "./guide-format";
 
 export const GUIDE_VIEWER_MANIFEST: Omit<GuideViewerAssets, "baseUrl"> = {
-  js: "viewer.48O72WuZ.js",
-  css: "viewer.Dn6ME03f.css",
-  jsIntegrity: "sha384-XQrIafIUrC1JvOSLEFyhqNObv7YmydTDPwXCDJvSV7UcU2BMouDtQADclC1uvAGK",
-  cssIntegrity: "sha384-w6E3c/iOdoRc9Zm/n1JmyXivgbVy8Vh6j9dzyIExsujQwXucakIKp225p8N20hWP",
+  js: "viewer.BWNKbsqg.js",
+  css: "viewer.CcruCQTB.css",
+  jsIntegrity: "sha384-noW3gnEjSW69GuRqOj8XEkZfAP+jaObsoN3Ncku9z44N8hgQctvqlya3LrKKHTgY",
+  cssIntegrity: "sha384-enWw9j1gqAlIappZ7TprCqx9ePNj+salKP81TCGxxqmU+HAxgeL8WU7RYMKemsXp",
   langs: {
     "astro": "chunks/astro.Ts5EKq2l.js",
     "c": "chunks/c.BIGW1oBm.js",
